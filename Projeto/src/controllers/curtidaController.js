9a787c75faf441d3986e9e7bbeed83ca11739d7c
Lucas-Neves-hub <lucas.neves@sptech.school>
2025-11-19 01:45:57 -0300

    UPDATE: funcao inserir deslike e validacao deslike funcionando porém ainda não integradas

--- Projeto/src/controllers/curtidaController.js
+++ Projeto/src/controllers/curtidaController.js
@@ -4,55 +4,43 @@ var curtidaModel = require("../models/curtidaModel");
 function insereCurtida(req, res) {
     var fkUsuario = req.body.fkUsuario;
     var idMusica = req.body.idMusica;
-    var curtida  = req.body.curtida;
+ 
 
-    curtidaModel.insereCurtida(curtida, idMusica, fkUsuario).then(function(resposta){
-        res.status(200).send("Curtida feita com sucesso");
-    }).catch(function(erro){
-        res.status(500).json(erro.sqlMessage);
-    })
+    curtidaModel.insereCurtida(idMusica, fkUsuario).then(resposta => {
+        res.status(200).json(resposta);
+    }).catch(erro => {
+        console.error(erro);
+        res.status(500).json(erro);
+    });
 }
 
 function insereDescurtida(req, res) {
-  var fkUsuario = req.body.fkUsuario;
+   var fkUsuario = req.body.fkUsuario;
     var idMusica = req.body.idMusica;
-    var descurtida  = req.body.descurtida;
 
-    curtidaModel.insereDescurtida(descurtida, idMusica, fkUsuario).then(function(resposta){
-        res.status(200).send("Descurtida feita com sucesso");
-    }).catch(function(erro){
-        res.status(500).json(erro.sqlMessage);
-    })
+    curtidaModel.insereDescurtida(idMusica, fkUsuario)
+        .then(resposta => {
+            res.status(200).json(resposta);
+        })
+        .catch(erro => {
+            console.error(erro);
+            res.status(500).json(erro);
+        });
 }
-
 function validacao(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-   var idMusica = req.body.idMusica;
-    var fkUsuario = req.body.fkUsuario;
+   var idMusica = req.query.idMusica;
+    var fkUsuario = req.query.fkUsuario;
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
         curtidaModel.validacao(idMusica, fkUsuario, "dislike")
-        .then((resultado) => {
-        if(resultado.length > 0){
-            return res.status(403).json({
-                    mensagem: "Usuário já descurtiu essa música."});
-            }else
-            curtidaModel.insereDescurtida('dislike', idMusica, fkUsuario)
-            .then(
-                function (resultado) {
-                    res.json(resultado)
-                    res.status(200).json({ mensagem: "Descurtida registrada!" });
-                })}
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar a validacao Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
+        .then(resposta => {
+             res.status(200).json(resposta);
+            })
+            .catch(erro => {
+                console.error(erro);
+                res.status(500).json(erro);
+            });
+        }
 
 module.exports = {
     insereCurtida,
--- Projeto/src/models/curtidaModel.js
+++ Projeto/src/models/curtidaModel.js
@@ -1,16 +1,16 @@
 var database = require("../database/config")
 
 
-function insereCurtida(curtida, idMusica, fkUsuario) {
+function insereCurtida(idMusica, fkUsuario) {
     var instrucao = `
-        INSERT INTO opiniao (fkUsuario, fkMusica, opiniao) VALUES ('${fkUsuario}','${idMusica}','${curtida}');
+        INSERT INTO opiniao (fkUsuario, fkMusica, opiniao) VALUES ('${fkUsuario}','${idMusica}','like');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
-function insereDescurtida(descurtida, idMusica, fkUsuario) {
+function insereDescurtida(idMusica, fkUsuario) {
     var instrucao = `
-        INSERT INTO opiniao (fkUsuario, fkMusica, opiniao) VALUES ('${fkUsuario}','${idMusica}','${descurtida}');
+        INSERT INTO opiniao (fkUsuario, fkMusica, opiniao) VALUES ('${fkUsuario}','${idMusica}','dislike');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -22,7 +22,7 @@ function validacao(idMusica, fkUsuario) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-     SELECT opiniao FROM opiniao where fkMusica = '${idMusica}' AND fkUsuario = '${fkUsuario}' AND opiniao = 'dislike';
+     SELECT opiniao FROM opiniao WHERE fkMusica = '${idMusica}' AND fkUsuario = '${fkUsuario}' AND opiniao = 'dislike';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);}
